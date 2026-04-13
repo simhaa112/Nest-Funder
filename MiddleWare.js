6af7a3ff63dd20d74e1e75ca25b4172ae589eb8f
Narasimha <simhaa112@users.noreply.github.com>
2026-04-13 16:32:30 +0530

    fixed curUser and mongodb

--- MiddleWare.js
+++ MiddleWare.js
@@ -53,7 +53,7 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
-    if (!listing.owner.equals(res.locals.locals.curUser._id)) {
+    if (!listing.owner.equals(res.locals.curUser._id)) {
         req.flash("error", "You are not the owner of this listing.");
         return res.redirect(`/listings/${id}`);
     }
@@ -83,7 +83,7 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     let { id, reviewId } = req.params;
     let review = await Review.findById(reviewId);
-    if (!review.author.equals(res.locals.locals.curUser._id)) {
+    if (!review.author.equals(res.locals.curUser._id)) {
         req.flash("error", "You are not the author of this review.");
         return res.redirect(`/listings/${id}`);
     }
