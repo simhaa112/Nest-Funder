1cc1754c2da97a71d838aa5c101cfa9e48dc7f48
Narasimha <simhaa112@users.noreply.github.com>
2026-04-13 12:43:31 +0530

    removed razorpay

--- MajorProject.js
+++ MajorProject.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-
+ 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -15,38 +15,37 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-
+ 
 const listingRoute = require("./routes/listingRoutes.js");
 const reviewRoute = require("./routes/reviewsRoutes.js");
 const userRoute = require("./routes/userRoutes.js");
-const paymentRoute = require("./routes/paymentRoutes.js");
-
+ 
 // MongoDB URL from environment variable
 const dbURL = process.env.ATLAS_DB_URL;
-
+ 
 // View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
-
+ 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-
+ 
 // Session store
 const store = MongoStore.create({
     mongoUrl: dbURL,
     crypto: {
         secret: process.env.SECRET,
     },
-    touchAfter: 24 * 60 * 60, // in seconds
+    touchAfter: 24 * 60 * 60,
 });
-
+ 
 store.on("error", (err) => {
     console.log("ERROR in MONGO SESSION STORE", err);
 });
-
+ 
 const sessionOptions = {
     store,
     secret: process.env.SECRET,
@@ -58,18 +57,18 @@ const sessionOptions = {
         httpOnly: true
     }
 };
-
+ 
 // Use session and flash
 app.use(session(sessionOptions));
 app.use(flash());
-
+ 
 // Passport setup
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
+ 
 // Global variables for templates
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
@@ -77,7 +76,7 @@ app.use((req, res, next) => {
     res.locals.curUser = req.user;
     next();
 });
-
+ 
 // Connect to MongoDB
 main()
     .then(() => {
@@ -85,35 +84,34 @@ main()
     }).catch((err) => {
         console.log(err);
     });
-
+ 
 async function main() {
     await mongoose.connect(dbURL);
 }
-
+ 
 // Root route
 app.get("/", (req, res) => {
     res.redirect("/listings");
 });
-
+ 
 // Routes
 app.use("/listings", listingRoute);
 app.use("/listings/:id/reviews", reviewRoute);
 app.use("/", userRoute);
-app.use("/payment", paymentRoute);
-
+ 
 // 404 handler
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
-
+ 
 // Error handler
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something Went Wrong" } = err;
     res.status(statusCode).render("listings/error.ejs", { err });
 });
-
+ 
 // Listen on Render port or local 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-   console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
